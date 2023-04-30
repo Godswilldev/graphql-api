fe4096880dd47c45197d0ec20b6059582910a088
Godswilldev <gimmex1@gmail.com>
2023-04-30 12:01:28 +0100

    @feat: starter code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,42 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { join } from "path";
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ProductModule } from "products/product.module";
+import { DirectiveLocation, GraphQLDirective } from "graphql";
+import { ApolloDriverConfig, ApolloDriver } from "@nestjs/apollo";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRoot({
+      logging: false,
+      type: "postgres",
+      synchronize: true,
+      entities: [],
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.LOCAL_DATABASE_PASSWORD,
+    }),
+    ProductModule,
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      playground: true,
+      autoSchemaFile: join(process.cwd(), "src/schema.gql"),
+      driver: ApolloDriver,
+      installSubscriptionHandlers: true,
+      buildSchemaOptions: {
+        directives: [
+          new GraphQLDirective({
+            name: "upper",
+            locations: [DirectiveLocation.FIELD_DEFINITION],
+          }),
+        ],
+      },
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
-import { AppModule } from 'app.module';
-import { NestFactory } from '@nestjs/core';
+import * as fs from "fs";
+import { AppModule } from "app.module";
+import { NestFactory, Reflector } from "@nestjs/core";
+import { GlobalErrorHandler } from "utils/globalErrorHandler";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
 
-async function bootstrap() {
+const bootstrap = async () => {
+  const PORT = process.env.PORT || 8989;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
+
+  app.enableCors({ credentials: true, origin: ["http://localhost:3000"] });
+  app.setGlobalPrefix("api/v1");
+
+  const config = new DocumentBuilder()
+    .setTitle("GRAPHQL API")
+    .setDescription("GRAPHQL API STARTER KIT")
+    .setVersion("1.0")
+    .addCookieAuth("graphql-api")
+    .addServer("http://localhost:8989")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/", app, document);
+  fs.writeFileSync("./swagger-documentation.json", JSON.stringify(document));
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalFilters(new GlobalErrorHandler());
+
+  await app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
+};
 bootstrap();
